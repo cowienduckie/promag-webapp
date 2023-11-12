926e71978f28c6f8cad172d8cfe92acfd911542e
Minh Tran <minhtran.2404@outlook.com>
2023-11-12 13:29:42 +0700

    fix: identity proxy

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,12 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       https: true,
       proxy: {
-        '/authority': {
+        '^/eventdirect/authority/.*': {
           target: env.VITE_IDENTITY_URL || 'http://127.0.0.1:5101',
           changeOrigin: true,
           secure: false,
           agent: new http.Agent(),
-          rewrite: (path) => path.replace(/^\/authority/, '')
+          rewrite: (path) => path.replace('/authority', '')
         },
         '/graphql': {
           target: env.VITE_GRAPHQL_URL || 'http://127.0.0.1:5100/graphql',
