edb8d40b1a54dc15927ea3feec0a683adafe65b6
Minh Tran <minhtran.2404@outlook.com>
2023-11-12 13:05:31 +0700

    chore: enable https

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,10 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
-  console.log('Identity Url: ' + env.VITE_IDENTITY_URL);
-
   return {
     server: {
       port: 3000,
+      https: true,
       proxy: {
         '/authority': {
           target: env.VITE_IDENTITY_URL || 'http://127.0.0.1:5101',
