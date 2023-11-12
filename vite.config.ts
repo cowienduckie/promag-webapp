e586894d55826d869b4f930e8d483b53d69b47f9
Minh Tran <minhtran.2404@outlook.com>
2023-11-12 11:35:42 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       proxy: {
         '/authority': {
-          target: env.VITE_IDENTITY_URL || 'http://127.0.0.1:5101',
+          target: env.VITE_IDENTITY_URL || 'https://id.promag.minhtrandev.com',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/authority/, '')
         },
