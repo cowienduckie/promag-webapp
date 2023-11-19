67a8589721d13178fe7c8f908a66a2eee5b77a6f
Minh Tran <minhtran.2404@outlook.com>
2023-11-19 11:47:57 +0700

    chore: enable HTTPS on dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -33,6 +34,6 @@ export default defineConfig(({ mode }) => {
         '@': path.resolve(__dirname, './src')
       }
     },
-    plugins: [react()]
+    plugins: [react(), mkcert()]
   };
 });
