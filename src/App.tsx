6400bbaf512849c52034ec1013fb4568f3bc7fa4
Minh Tran <minhtran.2404@outlook.com>
2024-01-29 02:51:43 +0700

    chore: add Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { RouterProvider } from 'react-router-dom';
 
 import { AppProvider } from '@/providers/app';
@@ -11,6 +12,7 @@ function App() {
         <RouterProvider router={AppRoutes()} />
       </AppProvider>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
